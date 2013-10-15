5a6c62cfa3328ee96c8ea08116e9edfa7efc9fa4
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-10-15 15:58:50 +0200

    Added dbus-proxy support

--- pelagicontain.c
+++ pelagicontain.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <signal.h>
 
+int DEBUG = 0;
+
 char *gen_ct_name ()
 {
 	struct timeval time;
@@ -27,7 +29,9 @@ char *gen_ct_name ()
 /* Spawn the proxy and use the supplied path for the socket */
 pid_t spawn_proxy (char *path, char *proxy_conf)
 {
-	printf ("Spawning proxy: %s\n", proxy_conf);
+	if (DEBUG)
+		printf ("Spawning proxy.. Socket: %s config: %s\n",
+		         path, proxy_conf);
 	pid_t pid = fork();
 	if (pid == 0) { /* child */
 		int exit_status = 0;
@@ -43,14 +47,20 @@ pid_t spawn_proxy (char *path, char *proxy_conf)
 
 int main (int argc, int **argv)
 {
-	char *container_name = NULL;
-	char *user_command   = NULL;
-	char *lxc_command    = NULL;
-	char *deploy_dir     = NULL;
-	int   max_cmd_len    = sysconf(_SC_ARG_MAX);
-	char  proxy_socket[1024];
-	char  proxy_config[1024];
-	pid_t proxy_pid      = 0;
+	char *container_name    = NULL;
+	char *user_command      = NULL;
+	char *lxc_command       = NULL;
+	char *deploy_dir        = NULL;
+	int   max_cmd_len       = sysconf(_SC_ARG_MAX);
+	pid_t session_proxy_pid = 0;
+	pid_t system_proxy_pid  = 0;
+	char  session_proxy_socket[1024];
+	char  system_proxy_socket[1024];
+	char  deployed_session_proxy_socket[1024];
+	char  deployed_system_proxy_socket[1024];
+	char  session_proxy_config[1024];
+	char  system_proxy_config[1024];
+	char  env[2048];
 
 	if (argc < 3) {
 		printf ("USAGE: %s [deploy directory] [command]\n", argv[0]);
@@ -62,11 +72,31 @@ int main (int argc, int **argv)
 	lxc_command    = malloc (sizeof (char) * max_cmd_len);
 	deploy_dir     = (char *)argv[1];
 
-	snprintf (proxy_socket, 1024, "%s/%s.sock", deploy_dir, container_name);
-	snprintf (proxy_config, 1024, "%s/proxy_config", deploy_dir);
+	snprintf (session_proxy_socket, 1024, "%s/sess_%s.sock", deploy_dir,
+	          container_name);
+	snprintf (system_proxy_socket, 1024, "%s/sys_%s.sock", deploy_dir,
+	          container_name);
+	snprintf (deployed_session_proxy_socket, 1024,
+	          "/deployed_app/sess_%s.sock", container_name);
+	snprintf (deployed_system_proxy_socket, 1024,
+	          "/deployed_app/sys_%s.sock", container_name);
+	snprintf (session_proxy_config, 1024, "%s/sess_proxy_config",
+	          deploy_dir);
+	snprintf (system_proxy_config, 1024, "%s/sess_proxy_config",
+	          deploy_dir);
+
+	/* Set up an environment */
+	strcat (env, "DBUS_SESSION_BUS_ADDRESS=unix:path=");
+	strcat (env, deployed_session_proxy_socket);
+	strcat (env, " ");
+	strcat (env, "DBUS_SYSTEM_BUS_ADDRESS=unix:path=");
+	strcat (env, deployed_system_proxy_socket);
 
 	/* Spawn proxy */
-	proxy_pid = spawn_proxy (proxy_socket, proxy_config);
+	session_proxy_pid = spawn_proxy (session_proxy_socket,
+	                                 session_proxy_config);
+	system_proxy_pid = spawn_proxy (system_proxy_socket, 
+	                                system_proxy_config);
 
 	/* Create container */
 	sprintf (lxc_command, "DEPLOY_DIR=%s lxc-create -n %s -t pelagicontain"
@@ -89,17 +119,22 @@ int main (int argc, int **argv)
 		strcat (user_command, " ");
 	}
 
-	sprintf (lxc_command, "lxc-execute -n %s -- %s",
-	                      container_name, user_command);
+	sprintf (lxc_command, "lxc-execute -n %s -- env %s %s",
+	                      container_name, env, user_command);
 	system (lxc_command);
 
 	/* Destroy container */
 	sprintf (lxc_command, "lxc-destroy -n %s", container_name);
 	system (lxc_command);
 
-	/* Terminate the proxy */
-	printf ("Killing proxy with pid %d\n", proxy_pid);
-	kill (proxy_pid, SIGTERM);
+	/* Terminate the proxy processes, remove sockets */
+	if (DEBUG)
+		printf ("Killing proxies with pids %d, %d\n",
+		        session_proxy_pid, system_proxy_pid);
+	kill (session_proxy_pid, SIGTERM);
+	kill (system_proxy_pid, SIGTERM);
+	remove (session_proxy_socket);
+	remove (system_proxy_socket);
 
 	/* .. and we're done! */
 	free (user_command);
