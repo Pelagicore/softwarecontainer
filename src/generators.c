9a6d797a1c44fa0c67c4a096a219c99b473fc363
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-11-06 14:20:14 +0100

    Added counter for IP addresses and documentation

--- src/generators.c
+++ src/generators.c
@@ -1,4 +1,25 @@
+/*
+ * Copyright (C) 2013, Pelagicore AB <jonatan.palsson@pelagicore.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
 #include "generators.h"
+
+const static char *iface_counter_file = "/tmp/pelc_ifc";
 
 char *gen_net_iface_name (char *ip_addr_net)
 {
@@ -16,11 +37,32 @@ char *gen_gw_ip_addr (char *ip_addr_net)
 
 char *gen_ip_addr (char *ip_addr_net)
 {
-	/* This is purely a proof of concept. Actually using this means we will
-	 * have conflicting IPs in the containers. This will mean out IPtables
-	 * rules are wrong */
-	char *ip = malloc (sizeof (char) * 20);
-	snprintf (ip, 20, "%s%d", ip_addr_net, (rand() % 253) + 1);
+	char *ip    = malloc (sizeof (char) * 20);
+	char *buf   = calloc(sizeof (char), 4);
+	int fd      = open (iface_counter_file, O_CREAT | O_RDWR);
+	int counter = 0;
+
+	if (fd == -1) {
+		printf ("Unable to lock interface counter\n");
+		return NULL;
+	}
+	flock (fd, LOCK_EX);
+
+	if (read (fd, buf, 3) == 0)
+		counter = 1;
+	else
+		counter = atoi (buf);
+
+	snprintf(buf, 4, "%03d", (counter % 254) + 1);
+
+	lseek (fd, 0, SEEK_SET);
+	write (fd, buf, 3);
+
+	flock(fd, LOCK_UN);
+	close (fd);
+
+	free (buf);
+	snprintf (ip, 20, "%s%d", ip_addr_net, counter);
 	return ip;
 }
 
--- src/generators.h
+++ src/generators.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013, Pelagicore AB <jonatan.palsson@pelagicore.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
 #ifndef GENERATORS_H
 #define GENERATORS_H
 
@@ -6,11 +25,23 @@
 #include "pelagicontain_common.h"
 #include "debug.h"
 #include "string.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "sys/file.h"
+#include "fcntl.h"
 
 char *gen_net_iface_name (char *ip_addr_net);
 
 char *gen_gw_ip_addr (char *ip_addr_net);
 
+/*! \brief Generate an IP address
+ *
+ * Calling this function will generate a new IP address. A counter is kept in
+ * /tmp to minimize the risk of collissons
+ *
+ * \param *ip_addr_net A 24 bit network portion of an IP address
+ * \return A string representing an IP address
+ */
 char *gen_ip_addr (char *ip_addr_net);
 
 char *gen_lxc_config (struct lxc_params *params);
