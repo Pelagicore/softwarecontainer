ce1a5a4f08543fa3e23bff3d4e593142c8fd13c1
Jacques GUILLOU <jacques.guillou@pelagicore.com>
2013-12-16 15:00:53 +0100

    Add pelagicore-utils based documentation

---
+++ doc/doxygen_mainpage.h
@@ -0,0 +1,24 @@
+/**
+
+\mainpage Pelagicontain
+
+\section Introduction
+
+This documentation describes the pelagicontain.....
+
+\section Content
+This package is made of the following components:
+        \li \ref Blabla 1
+        \li \ref Blabla 2
+
+
+\tableofcontents
+
+\section FAQ FAQ
+\li TODO
+
+\section ChangeLog Change log
+\include ChangeLog.txt
+
+*/
+
