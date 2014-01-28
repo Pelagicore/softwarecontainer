ae3612b1ec62ad78b65fa04d27413d09f0cf47c8
Joakim Gross <joakim.gross@pelagicore.com>
2014-01-28 16:20:48 +0100

    controller/main.cpp: Added protocol for Pelagicontain to communicate

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,38 +1,91 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include "errno.h"
 
+/**
+ * "Controller" is meant to be the interface Pelagicontain uses to reach the
+ * inside of the container. This implementation is just a stub to support the
+ * basic flow between components, consider it a test. Must be made more useful and robust!
+ */
 
+int runApp();
+void killApp();
+void shutdown();
 
-       #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <fcntl.h>
+pid_t pid;
+
 int main(int argc, char **argv)
 {
-// 	DBus::BusDispatcher dispatcher;
-// 	DBus::default_dispatcher = &dispatcher;
-// 	DBus::Connection bus = DBus::Connection::SessionBus();
-// 
-// 	bus.request_name("com.pelagicore.Controller");
-// 
-// 	Controller controller;
-// 	ControllerToDBusAdapter controllerAdapter(bus, controller);
-// 
-// 	dispatcher.enter();
-// 
 	std::cout << "In Controller" << std::endl;
+
+	/** The first command line arg to controller can be the path to where the
+	 *  FIFO file is. This is set to /deployed_app/ if it is launched with no
+	 *  argument (e.g. called by Pelagicontain), but for testing reasons it's
+	 *  convenient if the real path can be specified instead.
+	 */
+	std::string path;
+	if (argc == 2)
+		path = std::string(argv[1]);
+	else
+		path = std::string("/deployed_app/");
+	path += "in_fifo";
+
 	char c;
-// 	for (int i = 0; i < 300; i++) {
-// 		int status = read(0, &c, 1);
-	int fd = open("/deployed_app/in_fifo", O_RDONLY);
+	int fd = open(path.c_str(), O_RDONLY);
 	for (;;) {
-		
 		int status = read(fd, &c, 1);
-		if (status > 0)
-			std::cout << "read ststus is: " << status << c << errno << std::endl;
+		if (status > 0) {
+			switch (c) {
+			case '1':
+				runApp();
+				continue;
+			case '2':
+				killApp();
+				continue;
+			case '3':
+				shutdown();
+				continue;
+			case '\n':
+				// Ignore newline
+				continue;
+			default:
+				std::cout << "Controller didn't understand that" << std::endl;
+			}
+		}
 	}
-// 		std::cout << "read ststus is: " << status << c << errno << std::endl;
-// 	}
 	return 0;
+}
+
+int runApp()
+{
+	std::cout << "Will run app now..." << std::endl;
+	pid = fork();
+	if (pid == 0) { // Child
+		// This path to containedapp makes sense inside the container
+		int ret = execlp("/deployed_app/containedapp", "containedapp", NULL);
+		std::cout << "#### We should not be here! execlp: " << ret << " " << errno << std::endl;
+		exit(0);
+	} // Parent
+	std::cout << "#### pid: " << "\"" << pid << "\"" << std::endl;
+	return pid;
+}
+
+void killApp()
+{
+	std::cout << "Trying to kill " << pid << std::endl;
+	kill(pid, SIGINT);
+	int status;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status)) {
+		std::cout << "Wait status for pid " << pid << ": " << WEXITSTATUS(status) << std::endl;
+	}
+}
+
+void shutdown()
+{
+	exit(0);
 }
