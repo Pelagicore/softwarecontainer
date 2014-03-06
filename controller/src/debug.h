d8e3f83ffebdd883ed8bb170a46f38b26d41b9d3
Joakim Gross <joakim.gross@pelagicore.com>
2014-03-06 11:31:15 +0100

    Added cmake support for unit-tests in both controller and pelagicontain. In order to build unit tests for both components by invoking the same chain of cmake listfiles there were some things that had to be moved around. Also added minimal code to build test placeholder in controller/unit-test.

---
+++ controller/src/debug.h
@@ -0,0 +1,15 @@
+/*
+ *   Copyright (C) 2014 Pelagicore AB
+ *   All rights reserved.
+ */
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "pelagicore-log.h"
+
+/*! Imports the default log context
+ */
+
+LOG_IMPORT_DEFAULT_CONTEXT(Controller_DefaultLogContext);
+
+#endif /* DEBUG_H */
---
+++ controller/unit-test/main.cpp
@@ -0,0 +1,19 @@
+/*
+ *   Copyright (C) 2014 Pelagicore AB
+ *   All rights reserved.
+ */
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "debug.h"
+
+LOG_DEFINE_APP_IDS("CON", "Controller");
+LOG_DECLARE_CONTEXT(Controller_DefaultLogContext, "CON", "Main context");
+
+int main(int argc, char **argv) {
+    ConsoleLogOutput logOuput("/dev/null");
+    ConsoleLogOutput::setInstance(logOuput);
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
