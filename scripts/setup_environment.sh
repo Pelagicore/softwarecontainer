9f5d08f3b71b9dd47f44f61a2f9faa7b00ec01ba
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2014-03-17 13:14:51 +0100

    Updated setup_environment script

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,4 +1,65 @@
 #!/usr/bin/env bash
-echo "Setting up environment in $1..."
-ROOTFSDIR=$1/rootfs/
+
+# README:
+# -c: clean, this means the directory specified with -d should be removed
+#     before creating it again
+# -d: deployment directory, where to place container files
+#
+# Additional arguments: Files to put in directory specified with -d
+#
+# Example: ./setup_environment.sh -d /tmp/container -c yes -- /etc/passwod
+
+BRCTL_CMD="/sbin/brctl"
+BRIDGE="br0"
+
+
+# Parse command line arguments
+clean= deploydir=
+
+while getopts c:d: opt; do
+    case $opt in
+    c)
+        clean=$OPTARG
+        ;;
+    d)
+        deploydir=$OPTARG
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# Set deployment directory
+if [[ -z "$deploydir" ]]; then
+    echo "Must specify -d (deployment directory)"
+    exit
+fi
+
+# Check if deploy directory should be cleaned
+if [ "$clean" == "yes" ]; then
+    if [ -d "$deploydir/rootfs" ]; then
+        echo "Removing $deploydir"
+        rm -rf $deploydir
+    else
+        echo "This does not appear to be a deployment directory (rootfs/
+              missing)"
+    fi
+fi
+
+# Set up system
+echo "Checking system prerequisites..."
+if [[ -n $($BRCTL_CMD show | grep $BRIDGE) ]]; then
+    echo "Found $BRIDGE"
+else
+    echo "$BRIDGE was NOT FOUND, attempting to add..."
+    sudo brctl addbr $BRIDGE
+fi
+
+# Create directory structure
+echo "Setting up environment in $rootfsdir..."
+ROOTFSDIR=$deploydir/rootfs/
 mkdir -p $ROOTFSDIR
+
+# Deploy files in rootfs/
+for file in $@; do
+    cp $file $ROOTFSDIR
+done
