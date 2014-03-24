7e5f9b3aff363ecf28c49065d06de63f325adc70
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2014-03-24 12:22:11 +0100

    Added unit-test stub for devicenodegateway

---
+++ pelagicontain/unit-test/devicenodegateway_unittest.cpp
@@ -0,0 +1,51 @@
+/*
+ *   Copyright (C) 2014 Pelagicore AB
+ *   All rights reserved.
+ */
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "debug.h"
+#include "log_console.h"
+
+#include "devicenodegateway.h"
+
+using namespace pelagicore;
+
+class MockController :
+    public ControllerAbstractInterface
+{
+public:
+
+    virtual bool startApp()
+    {
+            return true;
+    }
+
+    virtual bool shutdown()
+    {
+        return true;
+    }
+
+    virtual bool setEnvironmentVariable(const std::string &variable,
+        const std::string &value)
+    {
+        return true;
+    }
+
+    MOCK_METHOD1(systemCall,
+        bool(const std::string &cmd));
+};
+
+using ::testing::InSequence;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::NiceMock;
+
+TEST(DeviceNodeGatewayTest, TestIdEqualsdevicenode) {
+    /* Nice mock, i.e. don't warn about uninteresting calls on this mock */
+    NiceMock<MockController> controllerInterface;
+    DeviceNodeGateway gw(&controllerInterface);
+
+    ASSERT_STREQ(gw.id().c_str(), "devicenode");
+}
