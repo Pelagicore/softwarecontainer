3e62f116c9dd96c5098c303a5bd09fcd5114d0e0
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2014-03-28 10:25:13 +0100

    Enforced PEP8 in test_devicenodegateway.py

--- pelagicontain/component-test/test_devicenodegateway.py
+++ pelagicontain/component-test/test_devicenodegateway.py
@@ -4,18 +4,21 @@
     Copyright (C) 2014 Pelagicore AB
     All rights reserved.
 """
-import json, os, sys, time, re
+import json
+import sys
+import time
+import re
 
 from common import ComponentTestHelper
 
-config = { "devices": [
+config = {"devices": [
     {
         "name": "/dev/random",
         "major": "1",
         "minor": "8",
         "mode": "666"
     }
-] }
+]}
 
 configs = {
     "devicenode": json.dumps(config)
@@ -24,25 +27,28 @@ configs = {
 helper = ComponentTestHelper()
 container_root = None
 
+
 # Check that sys.argv is usable, notify user otherwise. Exits on failure
-def ensureCommandLineOK():
+def ensure_command_line_ok():
     if len(sys.argv) != 3:
         print "Proper invocation looks like this:"
         print "%s <pelagicontain path> <container path>" % sys.argv[0]
         sys.exit(1)
 
+
 # Run before any tests
 def setup():
     global container_root
-    ensureCommandLineOK()
+    ensure_command_line_ok()
     pelagicontain_binary = sys.argv[1]
     container_root = sys.argv[2]
 
     helper.pam_iface.helper_set_configs(configs)
-    if  not helper.start_pelagicontain2(pelagicontain_binary, container_root,
-                                "/deployed_app/controller"):
+    if not helper.start_pelagicontain2(pelagicontain_binary, container_root,
+                                       "/deployed_app/controller"):
         print "Failed to launch pelagicontain!"
         sys.exit(1)
+
 
 # Run after all tests
 def teardown():
@@ -64,13 +70,11 @@ helper.pam_iface.helper_trigger_update(helper.cookie, configs)
 
 time.sleep(2)
 
-with open ("%s/rootfs/devicenode_test_output" % container_root) as f:
+with open("%s/rootfs/devicenode_test_output" % container_root) as f:
     line = f.readline()
     print "-->", line
-    if re.match (
-        "\s*\d\scrw-rw-rw-\s*\d\s*root\s*root\s*\d,\s*\d.*/dev/random$", line):
+    regex = "\s*\d\scrw-rw-rw-\s*\d\s*root\s*root\s*\d,\s*\d.*/dev/random$"
+    if re.match(regex, line):
         print "Looks like device was created OK!"
 
 teardown()
-
-
