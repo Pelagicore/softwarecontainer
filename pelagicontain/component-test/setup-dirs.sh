b870b9ad9915eb5153859883c4a01103bb1f2896
Erik Botö <erik.boto@pelagicore.com>
2014-03-31 09:58:20 +0200

    setup-dirs.sh: remove "comptest" from path

--- pelagicontain/component-test/setup-dirs.sh
+++ pelagicontain/component-test/setup-dirs.sh
@@ -3,14 +3,14 @@
 echo "This script will set up a component test directory structure in $1"
 
 if [ ! -z $1 ]; then
-	mkdir -p $1/comptest/bin
-	mkdir -p $1/comptest/com.pelagicore.comptest/bin
-	mkdir -p $1/comptest/com.pelagicore.comptest/shared
-	mkdir -p $1/comptest/com.pelagicore.comptest/home
-	mkdir -p $1/comptest/late_mounts
+	mkdir -p $1/bin
+	mkdir -p $1/com.pelagicore.comptest/bin
+	mkdir -p $1/com.pelagicore.comptest/shared
+	mkdir -p $1/com.pelagicore.comptest/home
+	mkdir -p $1/late_mounts
 
-	sudo mount --bind $1/comptest/late_mounts $1/comptest/late_mounts
-	sudo mount --make-unbindable $1/comptest/late_mounts
-	sudo mount --make-shared $1/comptest/late_mounts
+	sudo mount --bind $1/late_mounts $1/late_mounts
+	sudo mount --make-unbindable $1/late_mounts
+	sudo mount --make-shared $1/late_mounts
 fi
 
