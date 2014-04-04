0d7e367d0352bf4c1f00957531625f2345105f5a
Kristofer Hansson Aspman <kristofer.hansson-aspman@pelagicore.com>
2014-04-04 09:21:49 +0200

    unit-tests: Added tests for pulsegateway

---
+++ pelagicontain/unit-test/pulsegateway_unittest.cpp
@@ -0,0 +1,177 @@
+/*
+ *   Copyright (C) 2014 Pelagicore AB
+ *   All rights reserved.
+ */
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "pulsegateway.h"
+#include "pulsegateway_unittest_data.h"
+#include "generators.h"
+#include "debug.h"
+#include "log_console.h"
+
+using namespace pelagicore;
+
+class PulseMockController:
+    public ControllerAbstractInterface
+{
+public:
+
+    virtual bool startApp()
+    {
+            return true;
+    }
+
+    virtual bool shutdown()
+    {
+        return true;
+    }
+
+    MOCK_METHOD1(systemCall,
+        bool(const std::string &cmd));
+
+    MOCK_METHOD2(setEnvironmentVariable,
+                 bool(const std::string &variable, const std::string &value));
+};
+
+
+class MockSystemcallInterface:
+    public SystemcallAbstractInterface
+{
+public:
+    virtual bool makeCall(const std::string &cmd, int &exitCode)
+    {
+        exitCode = 0;
+        return true;
+    }
+
+    MOCK_METHOD1(makeCall, bool(const std::string &cmd));
+
+};
+
+using ::testing::DefaultValue;
+using ::testing::InSequence;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::NiceMock;
+using ::testing::StrictMock;
+using ::testing::StrEq;
+using ::testing::A;
+
+class PulseGatewayTest:
+    public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        DefaultValue<bool>::Set(true);
+
+        // ContainerName and gatewayDir must be passed to constructor
+        containerName = "fake-containerName";
+        containerRoot = "/fake-directory/";
+
+        containerDir = containerRoot + "/" + containerName;
+        gatewayDir = containerDir + "/gateways";
+    }
+
+    virtual void TearDown()
+    {
+        using ::testing::DefaultValue;
+        DefaultValue<bool>::Clear();
+    }
+
+    NiceMock<PulseMockController> controllerInterface;
+    std::string containerName;
+    std::string gatewayDir;
+    std::string containerRoot;
+    std::string containerDir;
+};
+
+/*! Test that id is correct ("pulseaudio")
+ */
+TEST_F(PulseGatewayTest, TestIdEqualspulseaudio) {
+    PulseGateway gw(gatewayDir, containerName, &controllerInterface);
+    ASSERT_STREQ(gw.id().c_str(), "pulseaudio");
+}
+
+TEST_F(PulseGatewayTest, TestHasNoEnvironment) {
+    PulseGateway gw(gatewayDir, containerName, &controllerInterface);
+
+    ASSERT_STREQ(gw.environment().c_str(), "");
+}
+
+/*! Test well-formed configuration that enables the gateway.
+ *  
+ *  Tests with configurations that are syntactically correct, i.e. not
+ *  causing errors, and that enables audio.
+ */
+class PulseGatewayValidConfig:
+    public testing::TestWithParam<pulseTestData> {};
+
+INSTANTIATE_TEST_CASE_P(InstantiationName, PulseGatewayValidConfig,
+                        ::testing::ValuesIn(validConfigs));
+
+TEST_P(PulseGatewayValidConfig, TestCanParseValidConfig) {
+    StrictMock<PulseMockController> controllerInterface;
+    PulseGateway gw("fake-gatewayDir", "fake-containerName", &controllerInterface);
+
+    struct pulseTestData config = GetParam();
+
+    DefaultValue<bool>::Set(true);
+    EXPECT_CALL(controllerInterface, setEnvironmentVariable(
+        A<const std::string &>(),
+        A<const std::string &>()));
+
+    bool success = gw.setConfig(config.data);
+    DefaultValue<bool>::Clear();
+    ASSERT_TRUE(success);
+
+    success = gw.activate();
+    ASSERT_TRUE(success);
+}
+
+
+/*! Test malformed configuration that disables the gateway.
+ *  
+ *  Tests with configurations that are syntactically incorrect, i.e.
+ *  causing parsing errors.
+ */
+class PulseGatewayInvalidConfig:
+    public testing::TestWithParam<pulseTestData> {};
+
+INSTANTIATE_TEST_CASE_P(InstantiationName, PulseGatewayInvalidConfig,
+                        ::testing::ValuesIn(invalidConfigs));
+
+TEST_P(PulseGatewayInvalidConfig, TestCanParseInvalidConfig) {
+    StrictMock<PulseMockController> controllerInterface;
+    PulseGateway gw("fake-gatewayDir", "fake-containerName", &controllerInterface);
+
+    struct pulseTestData config = GetParam();
+
+    bool success = gw.setConfig(config.data);
+    ASSERT_TRUE(!success);
+}
+
+/*! Test well-formed configuration that disables the gateway.
+ *  
+ *  Tests with configurations that are syntactically correct, i.e. not
+ *  causing errors, but that disables audio.
+ */
+class PulseGatewayDisablingConfig:
+    public testing::TestWithParam<pulseTestData> {};
+
+INSTANTIATE_TEST_CASE_P(InstantiationName, PulseGatewayDisablingConfig,
+                        ::testing::ValuesIn(disablingConfigs));
+
+TEST_P(PulseGatewayDisablingConfig, TestCanParseDisablingConfig) {
+    StrictMock<PulseMockController> controllerInterface;
+    PulseGateway gw("fake-gatewayDir", "fake-containerName", &controllerInterface);
+
+    struct pulseTestData config = GetParam();
+
+    EXPECT_CALL(controllerInterface, setEnvironmentVariable(_, _)).Times(0);
+
+    bool success = gw.setConfig(config.data);
+    ASSERT_TRUE(success);
+}
---
+++ pelagicontain/unit-test/pulsegateway_unittest_data.h
@@ -0,0 +1,75 @@
+
+struct pulseTestData {
+    const char *title;
+    const char *data;
+    const std::string audio;
+};
+
+/* void PrintTo(const pulseTestData& d, ::std::ostream* os) { */
+/*     *os << "Title: " << d.title; */
+/*     *os << "\nData: " << d.data; */
+/* } */
+
+const struct pulseTestData invalidConfigs[] = {
+    {
+        "Missing closing }",
+        "{\"audio\":  \"true\"",
+        "true"
+    },
+    {
+        "Missing value",
+        "{\"audio\": }",
+        "true"
+    },
+    {
+        "Missing key",
+        "{\"\": \"true\"}",
+        "true"
+    },
+    {
+        "Bad key",
+        "{\"audo\": }",
+        "true"
+    }
+};
+
+const struct pulseTestData validConfigs[] = {
+    {
+        "Minimal config",
+        "{"
+            "\"audio\": \"true\""
+        "}",
+        "true"
+    },
+    {
+        "Confusing config",
+        "{"
+            "\"audo\": \"true\","
+            "\"audio\": \"true\""
+        "}",
+        "true"
+    }
+};
+
+const struct pulseTestData disablingConfigs[] = {
+    {
+        "Minimal config",
+        "{"
+            "\"audio\": \"false\""
+        "}",
+        "false"
+    },
+    {
+        "Confusing config",
+        "{"
+            "\"audo\": \"true\","
+            "\"audio\": \"false\""
+        "}",
+        "false"
+    },
+    {
+        "Incorrect value",
+        "{\"audio\":  \"faulty-value\" }",
+        "false"
+    }
+};
