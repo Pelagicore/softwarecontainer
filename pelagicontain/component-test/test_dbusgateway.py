9c125d00056061780441b28c6ae188a00a3680ab
Richard Pannek <richard.pannek@pelagicore.com>
2014-05-27 10:37:59 +0200

    dbusgateway: added more tests

--- pelagicontain/component-test/test_dbusgateway.py
+++ pelagicontain/component-test/test_dbusgateway.py
@@ -33,9 +33,9 @@ CONFIG = {
     "dbus-proxy-config-system": [
         {
             "direction": "outgoing",
-            "interface": "com.pelagicore.comptest",
+            "interface": "org.freedesktop.DBus.Introspectable",
             "object-path": "/",
-            "method": "Echo"
+            "method": "Introspect"
         }
     ]
 }
@@ -44,11 +44,26 @@ CONFIGS = {
     "dbus-proxy": json.dumps(CONFIG)
 }
 
+CONFIG_WITHOUT_PERMISSION = {
+    "dbus-proxy-config-session": [],
+    "dbus-proxy-config-system": []
+}
+
+CONFIGS_WITHOUT_PERMISSION = {
+    "dbus-proxy": json.dumps(CONFIG_WITHOUT_PERMISSION)
+}
+
+CONFIGS_WITH_ERROR = {
+    "dbus-proxy": "{}"
+}
+
 # The app introspects PAM on D-Bus using dbus-send and writes the output to file
 # which content is used later in the assertion
 DBUSSEND_APP = """
 #!/bin/sh
-/appbin/dbus-send --session --print-reply --dest=com.pelagicore.PAM /com/pelagicore/PAM org.freedesktop.DBus.Introspectable.Introspect > /appshared/dbus_test_output
+
+/appbin/dbus-send --session --print-reply --dest=com.pelagicore.PAM /com/pelagicore/PAM org.freedesktop.DBus.Introspectable.Introspect > /appshared/dbus_test_output-session
+/appbin/dbus-send --system --print-reply --dest=org.freedesktop.DBus / org.freedesktop.DBus.Introspectable.Introspect > /appshared/dbus_test_output-system
 """
 
 # We keep the helper object module global so external fixtures can access it
@@ -60,27 +75,71 @@ class TestDBusGateway():
     def test_can_introspect(self, pelagicontain_binary, container_path, teardown_fixture):
         """ Tests Introspect() on com.pelagicore.PAM on D-Bus session bus.
         """
-        self.do_setup(pelagicontain_binary, container_path)
-        containedapp_file = container_path + "/com.pelagicore.comptest/bin/containedapp"
-        with open(containedapp_file, "w") as f:
-            print "Overwriting containedapp..."
-            f.write(DBUSSEND_APP)
-        os.system("chmod 755 " + containedapp_file)
-
-        helper.pelagicontain_iface().Launch(helper.app_id())
-        time.sleep(0.5)
+        self.do_setup(pelagicontain_binary, container_path, CONFIGS)
 
         try:
-            with open("%s/com.pelagicore.comptest/shared/dbus_test_output" % container_path) as f:
-                line = f.readline()
-                regex = "^method"
-                assert re.match(regex, line)
+            # check session output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-session" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="com\.pelagicore\.PAM">'
+                assert re.search(regex, xml)
+
+            # check system output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-system" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="org\.freedesktop\.DBus">'
+                assert re.search(regex, xml)
+
         except:
-            print "Output file not found"
+            print sys.exc_info()[0]
             exit(1)
 
-    def do_setup(self, pelagicontain_binary, container_path):
-        helper.pam_iface().helper_set_configs(CONFIGS)
+    def test_can_not_introspect(self, pelagicontain_binary, container_path, teardown_fixture):
+        """ Tests Introspect() on com.pelagicore.PAM on D-Bus session bus.
+        """
+        self.do_setup(pelagicontain_binary, container_path, CONFIGS_WITHOUT_PERMISSION)
+
+        try:
+            # check session output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-session" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="com\.pelagicore\.PAM">'
+                assert None == re.search(regex, xml)
+
+            # check system output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-system" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="org\.freedesktop\.DBus">'
+                assert None == re.search(regex, xml)
+
+        except:
+            print sys.exc_info()[0]
+            exit(1)
+
+    def test_can_not_introspect_with_error(self, pelagicontain_binary, container_path, teardown_fixture):
+        """ Tests Introspect() on com.pelagicore.PAM on D-Bus session bus.
+        """
+        self.do_setup(pelagicontain_binary, container_path, CONFIGS_WITH_ERROR)
+
+        try:
+            # check session output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-session" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="com\.pelagicore\.PAM">'
+                assert None == re.search(regex, xml)
+
+            # check system output
+            with open("%s/com.pelagicore.comptest/shared/dbus_test_output-system" % container_path) as f:
+                xml = f.read()
+                regex = '<interface name="org\.freedesktop\.DBus">'
+                assert None == re.search(regex, xml)
+
+        except:
+            print sys.exc_info()[0]
+            exit(1)
+
+    def do_setup(self, pelagicontain_binary, container_path, configs):
+        helper.pam_iface().helper_set_configs(configs)
         if not helper.start_pelagicontain(pelagicontain_binary, container_path):
             print "Failed to launch pelagicontain!"
             sys.exit(1)
@@ -95,3 +154,12 @@ class TestDBusGateway():
 
         print "Copying %s to %s" % (dbus_send, dest)
         shutil.copy(dbus_send, dest)
+
+        containedapp_file = container_path + "/com.pelagicore.comptest/bin/containedapp"
+        with open(containedapp_file, "w") as f:
+            print "Overwriting containedapp..."
+            f.write(DBUSSEND_APP)
+        os.system("chmod 755 " + containedapp_file)
+
+        helper.pelagicontain_iface().Launch(helper.app_id())
+        time.sleep(0.5) # sleep so the dbus interface has time to launch, etc.
