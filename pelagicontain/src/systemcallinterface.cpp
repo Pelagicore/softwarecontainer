3a2fe1842f7d20699f7bd1d70828b4255dba950a
Erik Botö <erik.boto@pelagicore.com>
2014-05-28 08:11:28 +0200

    systemcallinterface: Add check before trying to close file descriptors
    If filedescriptor is -1, do not try to close it. Used by e.g. dbusgateway so
    it can close the stdin FD after writing the config triggering a EOF in the
    child process.

--- pelagicontain/src/systemcallinterface.cpp
+++ pelagicontain/src/systemcallinterface.cpp
@@ -83,11 +83,15 @@ pid_t SystemcallInterface::makePopenCall(const std::string &command,
 
 bool SystemcallInterface::makePcloseCall(pid_t pid, int infp, int outfp)
 {
-    if (close(infp) == -1) {
-        log_error ("Failed to close STDIN to dbus-proxy");
+    if (infp > -1) {
+        if (close(infp) == -1) {
+            log_warning ("Failed to close STDIN to dbus-proxy");
+        }
     }
-    if (close(outfp) == -1) {
-        log_error ("Failed to close STDOUT to dbus-proxy");
+    if (outfp != -1) {
+        if (close(outfp) == -1) {
+            log_warning ("Failed to close STDOUT to dbus-proxy");
+        }
     }
 
     // the negative pid mekes it to kill the whole group, not only the shell
