5e2f1b017a73c1d45c5af63dc489c73ce25ce577
Joakim Gross <joakim.gross@pelagicore.com>
2014-07-16 12:18:13 +0200

    Shut down PC if container can't be set up correctly for a specific app. If Container::setApplication fails we should not go on and register with PAM and launch the app in a broken container, we should fail instead. Had to disable a unit test that relies on a different behaviour from Container.

--- pelagicontain/src/container.cpp
+++ pelagicontain/src/container.cpp
@@ -4,27 +4,24 @@
  */
 #include <fstream>
 #include <unistd.h>
-#include "container.h"
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include "container.h"
 
 #ifndef LXCTEMPLATE
     #error Must define LXCTEMPLATE as path to lxc-pelagicontain
 #endif
 
-/*
- * A directory need to exist and be set up in a special way.
+/* A directory need to exist and be set up in a special way.
  *
  * Basically something like:
  * mkdir -p <containerRoot>/late_mounts
  * mount --bind <containerRoot>/late_mounts <containerRoot>/late_mounts
  * mount --make-unbindable <containerRoot>/late_mounts
  * mount --make-shared <containerRoot>/late_mounts
- *
  */
-
 
 Container::Container(const std::string &name,
                      const std::string &configFile,
@@ -35,8 +32,7 @@ Container::Container(const std::string &name,
     m_mountDir(containerRoot + "/late_mounts")
 {
     // Make sure m_mountDir exist
-    if (!isDirectory(m_mountDir.c_str()))
-    {
+    if (!isDirectory(m_mountDir.c_str())) {
         log_error("Directory %s does not exist, shutting down.", m_mountDir.c_str());
         exit(-1);
     }
@@ -50,8 +46,7 @@ Container::Container(const std::string &name,
     allOk = allOk && createDirectory((runDir + "/shared").c_str());
     allOk = allOk && createDirectory((runDir + "/home").c_str());
 
-    if (!allOk)
-    {
+    if (!allOk) {
         log_error("Could not set up all needed directories, shutting down.");
         exit(-1);
     }
@@ -59,8 +54,7 @@ Container::Container(const std::string &name,
 
 bool Container::createDirectory(const std::string &path)
 {
-    if (mkdir(path.c_str(), S_IRWXU) == -1)
-    {
+    if (mkdir(path.c_str(), S_IRWXU) == -1) {
         log_error("Could not create directory %s, %s.", path.c_str(), strerror(errno));
         return false;
     }
@@ -73,10 +67,8 @@ bool Container::isDirectory(const std::string &path)
 {
     bool isDir = false;
     struct stat st;
-    if (stat(path.c_str(), &st) == 0)
-    {
-        if ((st.st_mode & S_IFDIR) != 0)
-        {
+    if (stat(path.c_str(), &st) == 0) {
+        if ((st.st_mode & S_IFDIR) != 0) {
             isDir = true;
         }
     }
@@ -91,8 +83,7 @@ Container::~Container()
          it != m_mounts.rend();
          ++it)
     {
-        if (umount((*it).c_str()) == -1)
-        {
+        if (umount((*it).c_str()) == -1) {
             log_error("Could not unmount %s, %s", (*it).c_str(), strerror(errno));
         }
     }
@@ -102,8 +93,7 @@ Container::~Container()
          it != m_dirs.rend();
          ++it)
     {
-        if (rmdir((*it).c_str()) == -1)
-        {
+        if (rmdir((*it).c_str()) == -1) {
             log_error("Could not remove dir %s, %s", (*it).c_str(), strerror(errno));
         }
     }
@@ -150,13 +140,12 @@ std::vector<std::string> Container::commands(const std::string &containedCommand
 bool Container::bindMountDir(const std::string &src, const std::string &dst)
 {
     int mountRes = mount(src.c_str(), // source
-                         dst.c_str(),  // target
-                         "",              // fstype
-                         MS_BIND,         // flags
-                         NULL);           // data
+                         dst.c_str(), // target
+                         "",          // fstype
+                         MS_BIND,     // flags
+                         NULL);       // data
 
-    if (mountRes == 0)
-    {
+    if (mountRes == 0) {
         // Success
         m_mounts.push_back(dst);
     } else {
@@ -170,23 +159,26 @@ bool Container::bindMountDir(const std::string &src, const std::string &dst)
     return (mountRes == 0);
 }
 
-/*
- * When we know which app that will be run we need to
+/* When we know which app that will be run we need to
  * do some setup, like mount in the application bin and
  * shared directories.
  */
-void Container::setApplication(const std::string &appId)
+bool Container::setApplication(const std::string &appId)
 {
     // The directory(ies) to be mounted is known by convention, e.g.
     // /var/am/<appId>/bin/ and /var/am/<appId>/shared/
-
 
     // bind mount /var/am/<appId>/bin/ into /var/am/late_mounts/<contid>/bin
     // this directory will be accessible in container as according to
     // the lxc-pelagicontain template
     std::string appDirBase = m_containerRoot + "/" + appId;
     std::string dstDirBase = m_mountDir + "/" + m_name;
-    bindMountDir(appDirBase + "/bin", dstDirBase + "/bin");
-    bindMountDir(appDirBase + "/shared", dstDirBase + "/shared");
-    bindMountDir(appDirBase + "/home", dstDirBase + "/home");
+
+    bool mountOk = true;
+
+    mountOk = bindMountDir(appDirBase + "/bin", dstDirBase + "/bin");
+    mountOk = mountOk & bindMountDir(appDirBase + "/shared", dstDirBase + "/shared");
+    mountOk = mountOk & bindMountDir(appDirBase + "/home", dstDirBase + "/home");
+
+    return mountOk;
 }
