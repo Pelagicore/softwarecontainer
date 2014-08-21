e3a5faf556e66eb86e8f1f0edcfdc827dc87d60c
Tobias Olausson <tobias.olausson@pelagicore.com>
2014-08-21 15:16:44 +0200

    The IP number generator no longer uses a file for its operation.

--- pelagicontain/unit-test/generators_unittest.cpp
+++ pelagicontain/unit-test/generators_unittest.cpp
@@ -6,20 +6,11 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 #include "generators.h"
 
 TEST(GeneratorTest, TestIPCounter) {
     Generator gen;
     const char ip_addr_net[] = "192.168.0.";
-    const char filename[] = "/tmp/pelc_ifc";
-    unlink(filename);
     
     // We start from 0 (no really, we start from 2)
     std::string ip = gen.gen_ip_addr(ip_addr_net);
@@ -29,11 +20,10 @@ TEST(GeneratorTest, TestIPCounter) {
     ip = gen.gen_ip_addr(ip_addr_net);
     ASSERT_EQ(ip, "192.168.0.3");
 
-    // Make sure it wraps around
-    int fd = open(filename, O_WRONLY);
-    const char buf[] = "253";
-    write(fd, buf, strlen(buf));
-    close(fd);
+    // Update so the counter is 253
+    for(int i = 3; i < 253; i++) {
+        gen.gen_ip_addr(ip_addr_net);
+    }
 
     ip = gen.gen_ip_addr(ip_addr_net);
     ASSERT_EQ(ip, "192.168.0.254");
