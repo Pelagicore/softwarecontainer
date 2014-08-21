adbfa942b67327606e2b5dadd2ea9b100a3237a5
Joakim Ceder <joakim.ceder@pelagicore.com>
2014-08-21 09:46:04 +0200

    mainpage.h: Improve documentation formatting

--- doc/mainpage.h
+++ doc/mainpage.h
@@ -162,11 +162,12 @@
    </code>
 
    <h2>Running the Pelagicontain component tests</h2>
+   A network bridge is used by the container, be careful when configuring the bridge,
+   check that no other bridge use the ip address. Also make sure that the default gateway is up and running. <br /> 
+   
    Add a container-br0 bridge: <br />
-   Be careful when configuring  the bridge, check that no other bridge use the ip address.
-   Also make sure that the default gateway is up and r
-   unning. <br />
-   <code>brctl addbr container-br0</code> <br />
+
+   <code>brctladdbr container-br0</code> <br />
    <code>brctl setfd container-br0 0</code> <br />
    <code>ifconfig container-br0 10.0.3.1 netmask 255.255.255.0 promisc up</code> <br />
    <code>iptables -t nat -A POSTROUTING -s 10.0.3.0/24 ! -d 10.0.3.0/24 -j MASQUERADE</code> <br />
