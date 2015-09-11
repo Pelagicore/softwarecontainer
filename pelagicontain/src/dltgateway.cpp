30fc39f27149324a37e67b6ad7b0fc1f35f8a430
Tobias Olausson <tobias.olausson@pelagicore.com>
2015-09-11 11:15:31 +0200

    deletes dltgateway, use filegateway for same functionality

--- pelagicontain/src/dltgateway.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *   Copyright (C) 2014 Pelagicore AB
- *   All rights reserved.
- */
-#include "dltgateway.h"
-
-// no clue why the following lines are needed
-constexpr const char *DLTGateway::SOCKET_FILE_NAME;
-constexpr const char *DLTGateway::DLT_SOCKET_FOLDER;
