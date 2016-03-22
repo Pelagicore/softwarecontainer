954040550ca691a8c9c801f7a1d4c69f3aae4748
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2016-03-22 07:39:02 +0100

    examples: Use dbus-send & system bus for the 'simple' example
    This makes the example easier to run on targets, where the gdbus command
    likely isnt available. Also default to the system bus, which seems to
    work well on targets - the bus is configurable in the launch.sh script,
    should the user want to change it.

--- examples/simple/launch.sh
+++ examples/simple/launch.sh
@@ -4,6 +4,15 @@
 # container, the example will launch simple.c in a container, and observe that
 # it is killed inside the container by the OOM killer.
 
+# Register the path of the script. This is what we will mount in to the
+# container later, in order to access the 'simple' binary.
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+echo "Using system bus -- configure this in $0"
+BUS="system"
+# Uncomment the line below to use the session bus
+#BUS="session"
+
 # Create a new session D-Bus bus
 eval `dbus-launch --sh-syntax`
 
@@ -11,28 +20,28 @@ eval `dbus-launch --sh-syntax`
 env
 
 # Launch a new agent
-../../build/agent/pelagicontain-agent &
+pelagicontain-agent &
 AGENTPID="$!"
 
 # Let the agent start up
 sleep 2
 
-export PCCMD="gdbus call --session -d com.pelagicore.PelagicontainAgent -o /com/pelagicore/PelagicontainAgent"
+export PCCMD="dbus-send --${BUS} --print-reply --dest=com.pelagicore.PelagicontainAgent /com/pelagicore/PelagicontainAgent"
 
 # Introspect the agent
-gdbus introspect --session -d com.pelagicore.PelagicontainAgent -o /com/pelagicore/PelagicontainAgent
+$PCCMD org.freedesktop.DBus.Introspectable.Introspect
 
 # Ping the agent
-$PCCMD -m com.pelagicore.PelagicontainAgent.Ping
+$PCCMD com.pelagicore.PelagicontainAgent.Ping
 
 # Create a new container
-$PCCMD -m com.pelagicore.PelagicontainAgent.CreateContainer prefix
+$PCCMD com.pelagicore.PelagicontainAgent.CreateContainer string:prefix
 
 # Expose a directory to the container
-$PCCMD -m com.pelagicore.PelagicontainAgent.BindMountFolderInContainer 0 /home/vagrant/pelagicontain/examples/simple app true
+$PCCMD com.pelagicore.PelagicontainAgent.BindMountFolderInContainer uint32:0 string:${SCRIPTPATH} string:app boolean:true
 
 # Run the simple example
-$PCCMD -m com.pelagicore.PelagicontainAgent.LaunchCommand 0 0 /gateways/app/simple /gateways/app/ /tmp/stdout "{}"
+$PCCMD com.pelagicore.PelagicontainAgent.LaunchCommand uint32:0 uint32:0 string:/gateways/app/simple string:/gateways/app/ string:/tmp/stdout dict:string:string:""
 
 # Let the example run for a while
 sleep 30
