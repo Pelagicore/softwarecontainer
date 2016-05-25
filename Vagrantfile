746f3d0aa9aac9bc3f1511bc048b12d9b3c3df0c
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-25 08:52:28 +0200

    Add pulseaudio for the unit tests

--- Vagrantfile
+++ Vagrantfile
@@ -21,6 +21,7 @@ Vagrant.configure(2) do |config|
     # Upgrade machine to testing distro & install build dependencies
     config.vm.provision "shell", path: "cookbook/deps/testing-upgrade.sh"
     config.vm.provision "shell", path: "cookbook/deps/common-build-dependencies.sh"
+    config.vm.provision "shell", path: "cookbook/deps/common-run-dependencies.sh"
 
     # Add known hosts
     config.vm.provision "shell", privileged: false, path: "cookbook/system-config/pelagicore-ssh-conf.sh"
--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,14 @@
 eval $(dbus-launch --sh-syntax)
 echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
 
+# TODO: pulseaudio needs to be setup properly for PelagicontainApp.TestPulseAudioEnabled to work
+pulseaudio --system &
+pid=$!
+
 ./libpelagicontain/unit-test/pelagicontainLibTest \
     --gtest_filter=-PelagicontainApp.FileGatewayReadOnly:PelagicontainApp.TestPulseAudioEnabled \
     --gtest_output=xml
 
-
+if ! kill $pid > /dev/null 2>&1 ; then
+    echo "Failed to kill pulseaudio at pid $pid"
+fi
