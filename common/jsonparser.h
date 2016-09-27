2fd5b92044454b4ee7a6eb07c67688b0f18f5b1c
roffe <viktor.sjolind@pelagicore.com>
2016-09-27 08:34:05 +0200

    Added documentation for jsonparser

--- common/jsonparser.h
+++ common/jsonparser.h
@@ -22,10 +22,42 @@
 #include <jansson.h>
 
 namespace softwarecontainer {
+
+    /**
+     * @brief Convenience layer for the Jansson JSON parser.
+     *
+     * This class provides convenience functions that handles commonly used operations
+     * using the Janssson JSON parser.
+     */
     class JSONParser {
 
     protected:
+        /**
+         * @brief Reads a string from a JSON Object.
+         *
+         * Tries to read a string value form a given JSON object using a given key.
+         * If the read is successful true will be returned and the result reference will be
+         * populated with the read value. If the string could not be read, either due to the
+         * given object does not contain the given key or the given key did not hold a string,
+         * false will be returned and the result reference will not be changed.
+         *
+         * @return True: If the string was successfully read.
+         * @return False: If the string could not be read.
+         */
         bool read(const json_t *element, const char *key, std::string &result);
+
+        /**
+         * @brief Reads a boolean from a JSON Object.
+         *
+         * Tries to read a boolean value form a given JSON object using a given key.
+         * If the read is successful true will be returned and the result reference will be
+         * populated with the read value. If the boolean could not be read, either due to the
+         * given object does not contain the given key or the given key did not hold a boolean
+         * value, false will be returned and the result reference will not be changed.
+         *
+         * @return True: If the integer was successfully read.
+         * @return False: If the integer could not be read.
+         */
         bool read(const json_t *element, const char *key, bool &result);
     };
 }
