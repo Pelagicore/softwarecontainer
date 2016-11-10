ff4c11448a8e6a636bdd992c885b719c9d643e28
Joakim Gross <joakim.gross@pelagicore.com>
2016-11-10 07:50:16 +0100

    Rewrite of service test env helper

--- servicetest/environment/testhelper.py
+++ servicetest/environment/testhelper.py
@@ -19,6 +19,8 @@
 import sys
 import os
 import json
+import argparse
+import abc
 
 
 """ This test helper is currently just used by the environment test suite(s).
@@ -28,6 +30,11 @@ import json
     many things basically the same way, and that using specific helpers
     for each suite will create a lot of almost-the-same helpers.
 
+    The helper is intended to be run as a program inside a container to drive
+    the tests, but also interfaced with directly from the tests when asserting
+    values. The code should therefore be considered with these two aspects as
+    the same code will have different purpose depending on where it's used.
+
     The intention of this helper going forwards (possibly) is to add command
     line args to be able to configure it runtime (when run in a container)
     to do what is needed. Let's see if future tests can use it like this,
@@ -35,9 +42,6 @@ import json
     suite(s).
 """
 
-
-# Accessed by users (i.e. tests and test suites)
-ENV_VARS_FILE_NAME = "env_vars_dump.json"
 
 # Set to True to have prints from the code
 VERBOSE = False
@@ -51,47 +55,109 @@ def LOG(message):
 
 
 class Helper(object):
-    """ The helper is run on the inside of containers to investigate
-        and interact with from within.
+    """ The helper is run on the inside of containers to investigate the inside
+        of the container, and interact with the outside from inside the container.
 
         This means that paths etc. are relative to how the container is
         configured, e.g. when creating it and what gateway configs are used.
+
+        The current design is assuming that most tests will want the helper to
+        write information to a file and later provide that info by reading it
+        back.
     """
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self, file_base_path):
-        """ file_base_path needs to make sense inside the container
+        """ file_base_path needs to make sense inside the container.
+
+            This path is either set when the helper is called as a program
+            to execute inside the container, or it's set by a test instantiating
+            the classes directly when the intention is to read back results from
+            the previous run.
         """
-        self.__base_path = file_base_path
+        self._base_path = file_base_path
+
+    @abc.abstractmethod
+    def write_result(self, data):
+        """ Write data to file.
+
+            What this method actually does is specific for each inheriting class,
+            which should all implement this method.
+        """
+        return
+
+
+class EnvironmentHelper(Helper):
+    """ Helper for examining the environment inside the container.
+    """
+
+    # Tests access this statically from e.g. fixtures
+    ENV_VARS_FILE_NAME = "env_vars_dump.json"
+
+    def __init__(self, file_base_path):
+        Helper.__init__(self, file_base_path)
+
+    #### Below methods are executed inside container
+    def write_result(self, data):
+        LOG("Will dump env vars to disk")
+        with open(self._base_path + "/" + self.__file_name(), "w") as fh:
+            fh.write(json.dumps(data.data))
 
     def get_env_vars(self):
         LOG("Getting all env vars")
         all_vars = os.environ
         return all_vars
 
-    def write_result(self, the_type, data):
-        LOG("Will write result")
-        if the_type == "env_vars":
-            LOG("Will dump env vars to disk")
-            with open(self.__base_path + "/" + ENV_VARS_FILE_NAME, "w") as fh:
-                fh.write(json.dumps(data.data))
+    #### Below methods are executed on the host (in the tests)
+    def env_var(self, name):
+        all_vars = self.__all_env_vars(self._base_path)
+        return all_vars[name]
+
+    #### Private methods - helpers etc.
+    def __all_env_vars(self, base_path):
+        all_vars = dict()
+        with open(base_path + "/" + self.__file_name(), "r") as fh:
+            all_vars = json.loads(fh.read())
+        return all_vars
+    
+    def __file_name(self):
+        """ Provide some de-coupling from the static-ness of the
+            file name.
+            TODO: This might be unnecessary...
+        """
+        return EnvironmentHelper.ENV_VARS_FILE_NAME
 
 
-def all_env_vars(base_path):
-    all_vars = dict()
-    with open(base_path + "/" + ENV_VARS_FILE_NAME, "r") as fh:
-        all_vars = json.loads(fh.read())
-    return all_vars
-
-
-def env_var(name, base_path):
-    all_vars = all_env_vars(base_path)
-    return all_vars[name]
-
+GET_ENV_VARS_OPTION = "get_env_vars"
 
 if __name__ == "__main__":
-    file_base_path = sys.argv[2]
+    """ When the program is called from command line it is running inside
+        a container.
+    """
 
-    h = Helper(file_base_path)
-    if sys.argv[1] == "--get-env-vars":
+    parser = argparse.ArgumentParser()
+
+    get_env_vars_help_message = \
+    """ A path to a file in which to store environment information.
+        This file is intended to be used by the helper when reading
+        data for asserting values.
+    """
+    parser.add_argument("--get-env-vars",
+                        nargs=1,
+                        action="store",
+                        dest=GET_ENV_VARS_OPTION,
+                        default=None,
+                        metavar="path",
+                        help=get_env_vars_help_message)
+
+    args = parser.parse_args()
+
+    file_base_path_parsed_value = getattr(args, GET_ENV_VARS_OPTION)
+    if file_base_path_parsed_value is not None:
+        # Extract the actual path string
+        file_base_path = file_base_path_parsed_value.pop()
+        h = EnvironmentHelper(file_base_path)
+        # Get the environment from inside the container
         env_vars = h.get_env_vars()
-        h.write_result("env_vars", env_vars)
+        # Dump the information for the helper to read back later in the tests
+        h.write_result(env_vars)
