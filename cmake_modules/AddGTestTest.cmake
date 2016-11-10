afc34b9e826f287d03f161febbb045ff5d3d8360
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-11-10 10:07:31 +0100

    No longer install the unit tests (gtest/gmock).
    We don't want to install the tests, we just want to run them, so
    we exclude the test library from install and we don't install
    our test targets either.
    
    Signed-off-by: Tobias Olausson <tobias.olausson@pelagicore.com>

--- cmake_modules/AddGTestTest.cmake
+++ cmake_modules/AddGTestTest.cmake
@@ -1,18 +1,12 @@
 macro(add_gtest_test testName files libraries)
 
-    if(NOT DEFINED TESTS_INSTALLATION_PATH)
-            set(TESTS_INSTALLATION_PATH "lib/${PROJECT_NAME}/ptest")
-    endif()
-
-    message(STATUS "Tests will be installed in : ${CMAKE_INSTALL_PREFIX}/${TESTS_INSTALLATION_PATH}")
-
-    add_executable(${testName}
-            ${files}
-    )
-
     if ((NOT TARGET gtest) OR (NOT TARGET gmock))
         message(FATAL_ERROR "No gtest or gmock targets found, can't add tests")
     else()
+        add_executable(${testName}
+            ${files}
+        )
+
         target_link_libraries(${testName}
             ${libraries}
             gtest
@@ -21,35 +15,10 @@ macro(add_gtest_test testName files libraries)
             gmock_main
         )
 
-        INSTALL(TARGETS ${testName}
-            DESTINATION ${TESTS_INSTALLATION_PATH}
-        )
-
         add_test(
             NAME ${testName}
             COMMAND ${testName}
         )
-
-        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/run-ptest "#!/bin/bash
-FOLDER=`dirname $0`
-for filename in $FOLDER/*; do
-        TEST_FILE=$filename
-        if [ \"`basename $filename`\" != \"run-ptest\" ]; then
-                if [[ -x $TEST_FILE ]]; then
-#			echo Starting $TEST_FILE
-                        TMP_FILE=`mktemp`
-                        # Run the test and convert its output to the ptest format
-                        $TEST_FILE >> $TMP_FILE
-                        cat $TMP_FILE | sed -r 's/^\\[\\s+OK\\s+\\] (.*) \\([0-9]+\\sms\\)$/PASS: \\1 /' \
-                            | sed -r 's/^\\[\\s+FAILED\\s+\\] (.*) \\([0-9]+\\sms\\)$/FAIL: \\1 /' \
-                            | awk '{if ($1 == \"PASS:\" || $1 == \"FAIL:\") {print $0}}'
-                fi
-        fi
-done
-")
-
-        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/run-ptest" DESTINATION ${TESTS_INSTALLATION_PATH}
-                PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ)
     endif()
 
 endmacro()
