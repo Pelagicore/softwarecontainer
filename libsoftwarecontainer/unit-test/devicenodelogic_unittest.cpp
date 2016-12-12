1f0445a7f730a25bcc60915f0fb5afac4e113226
kursat <kursat.kobya@pelagicore.com>
2016-12-12 16:03:45 +0100

    Add parameterized unit tests for DeviceNodeLogic class

--- libsoftwarecontainer/unit-test/devicenodelogic_unittest.cpp
+++ libsoftwarecontainer/unit-test/devicenodelogic_unittest.cpp
@@ -25,11 +25,51 @@
 #include <unistd.h>
 #include <libgen.h>
 
-class DeviceNodeLogicTest : public  ::testing::Test
+struct testModes
+{
+    int itemMode;
+    int newMode;
+    int expectedMode;
+};
+
+class DeviceModeTests : public  ::testing::TestWithParam<testModes>
 {
 public:
-    LOG_DECLARE_CLASS_CONTEXT("TEST", "Tester");
-    DeviceNodeLogicTest() { }
+    DeviceNodeLogic *dnl;
+    testModes testparams;
+
+    void SetUp() override
+    {
+        dnl = new DeviceNodeLogic();
+        testparams = GetParam();
+    }
+};
+
+/*
+ * This data is fed to the DeviceMode tests
+ */
+INSTANTIATE_TEST_CASE_P(DeviceModeParameters, DeviceModeTests, ::testing::Values(
+        testModes{644, 777 , 777},
+        testModes{444, 266 ,466},
+        testModes{444, 67  ,467},
+        testModes{644, 422 ,644},
+        testModes{20,  644 ,644},
+        testModes{666, 666 ,666}
+));
+
+
+/*
+ * Verify whitelisting in the case when mode is set to more permissive
+ */
+TEST_P(DeviceModeTests, UpdateDeviceMode) {
+    ASSERT_EQ(testparams.expectedMode,
+            dnl->calculateDeviceMode(testparams.itemMode,testparams.newMode));
+}
+
+class DeviceModeLogicFunctionsTests : public ::testing::Test
+{
+public:
+    DeviceModeLogicFunctionsTests() { };
     DeviceNodeLogic *dnl;
 
     void SetUp() override
@@ -39,85 +79,9 @@ public:
 };
 
 /*
- * Verify whitelisting in the case when mode is set to more permissive
- */
-TEST_F(DeviceNodeLogicTest, ChangeModeMorePermissive) {
-    const int itemMode = 644;
-    const int newMode  = 777;
-
-    const int expectedMode = 777;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
- * Verify whitelisting in the case when mode is set to more permissive
- * for some groups, but not all
- */
-TEST_F(DeviceNodeLogicTest, BothChangeAndNoChangeMode1) {
-    const int itemMode = 444;
-    const int newMode  = 266;
-
-    const int expectedMode = 466;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
- * Verify whitelisting in the case when mode is set to more permissive
- * for some groups, but not all
- */
-TEST_F(DeviceNodeLogicTest, BothChangeAndNoChangeMode2) {
-    const int itemMode = 444;
-    const int newMode  = 67;
-
-    const int expectedMode = 467;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
- * Verify whitelisting in the case when mode is set to less permissive,
- * implying no change will be made to the mode
- */
-TEST_F(DeviceNodeLogicTest, NoModeChangeLessPremissive1) {
-    const int itemMode = 644;
-    const int newMode  = 422;
-
-    const int expectedMode = 644;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
- * Verify whitelisting in the case when mode is set to less permissive,
- * implying no change will be made to the mode
- */
-TEST_F(DeviceNodeLogicTest, NoModeChangeLessPremissive2) {
-    const int itemMode = 20;
-    const int newMode  = 644;
-
-    const int expectedMode = 644;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
- * Verify whitelisting in the case when mode the same as before
- */
-TEST_F(DeviceNodeLogicTest, ModeEqual) {
-    const int itemMode  = 666;
-    const int newMode   = 666;
-
-    const int expectedMode = 666;
-
-    ASSERT_EQ(expectedMode, dnl->calculateDeviceMode(itemMode,newMode));
-}
-
-/*
  * Verify updateDeviceList and findDeviceByName functions working as expected
  */
-TEST_F(DeviceNodeLogicTest, findDeviceByNameFunction) {
+TEST_F(DeviceModeLogicFunctionsTests, findDeviceByNameFunction) {
     DeviceNodeParser::Device testDevice{"testDevice", 3, 5, 753};
     ASSERT_EQ(ReturnCode::SUCCESS, dnl->updateDeviceList(testDevice));
 
@@ -131,30 +95,40 @@ TEST_F(DeviceNodeLogicTest, findDeviceByNameFunction) {
     ASSERT_EQ("anotherTestDevice", device->name);
 }
 
+
+class UpdateDeviceListFailureTests : public  ::testing::TestWithParam<DeviceNodeParser::Device>
+{
+public:
+    DeviceNodeLogic *dnl;
+    DeviceNodeParser::Device testparams;
+
+    void SetUp() override
+    {
+        dnl = new DeviceNodeLogic();
+        testparams = GetParam();
+    }
+};
+
+/*
+ * This data is fed to the DeviceMode tests
+ */
+INSTANTIATE_TEST_CASE_P(DeviceParameters, UpdateDeviceListFailureTests, ::testing::Values(
+        DeviceNodeParser::Device{"testDevice", 4, 5, 777},
+        DeviceNodeParser::Device{"testDevice", 3, 46, 666},
+        DeviceNodeParser::Device{"testDevice", 4, 712, 555}
+));
+
 /*
  * Verify updateDeviceList function is failing as expected when user tries to add
  * another device with same name but different major ID
  */
-TEST_F(DeviceNodeLogicTest, failureMismatchingDevice) {
+TEST_P(UpdateDeviceListFailureTests, failureMismatchingDevice) {
     DeviceNodeParser::Device testDevice{"testDevice", 3, 5, 753};
     ASSERT_EQ(ReturnCode::SUCCESS, dnl->updateDeviceList(testDevice));
 
-    testDevice.name  = "testDevice";
-    testDevice.major = 4;
-    testDevice.mode  = 777;
-    ASSERT_EQ(ReturnCode::FAILURE, dnl->updateDeviceList(testDevice));
-}
-
-/*
- * Verify updateDeviceList function is failing as expected when user tries to add
- * another device with same name but different minor ID
- */
-TEST_F(DeviceNodeLogicTest, failureMismatchingDevice2) {
-    DeviceNodeParser::Device testDevice{"testDevice", 3, 5, 753};
-    ASSERT_EQ(ReturnCode::SUCCESS, dnl->updateDeviceList(testDevice));
-
-    testDevice.name  = "testDevice";
-    testDevice.minor = 44;
-    testDevice.mode  = 777;
+    testDevice.name  = testparams.name;
+    testDevice.major = testparams.major;
+    testDevice.minor = testparams.minor;
+    testDevice.mode  = testparams.mode;
     ASSERT_EQ(ReturnCode::FAILURE, dnl->updateDeviceList(testDevice));
 }
