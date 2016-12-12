46885cdc143c9870efbc45ee5ce4c8cc33f8ee76
Jenkins <noreply@pelagicore.com>
2016-12-12 16:43:08 +0100

    Built new docs on master

---
+++ api-docs/class_device_node_parser.js
@@ -0,0 +1,5 @@
+var class_device_node_parser =
+[
+    [ "Device", "struct_device_node_parser_1_1_device.html", "struct_device_node_parser_1_1_device" ],
+    [ "parseDeviceNodeGatewayConfiguration", "class_device_node_parser.html#ae2fd963670bbe718c167e61c506822c9", null ]
+];
