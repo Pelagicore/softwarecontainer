5004251406822464f9ae893c26918c43deaa22b1
Jenkins <noreply@pelagicore.com>
2016-12-13 09:26:46 +0100

    Built new docs on master

--- api-docs/dir_1e0d3fca9063d50ad596a6c51f9cebb4.js
+++
@@ -1,7 +0,0 @@
-var dir_1e0d3fca9063d50ad596a6c51f9cebb4 =
-[
-    [ "configstore.cpp", "configstore_8cpp_source.html", null ],
-    [ "configstore.h", "configstore_8h.html", [
-      [ "ConfigStore", "classsoftwarecontainer_1_1_config_store.html", "classsoftwarecontainer_1_1_config_store" ]
-    ] ]
-];
