e5a8d100a8744f48644b3028ed7ff9880f3705b4
Jenkins <noreply@pelagicore.com>
2017-01-30 15:31:15 +0100

    Built new docs on master

--- api-docs/classsoftwarecontainer_1_1_software_container_config.js
+++ api-docs/classsoftwarecontainer_1_1_software_container_config.js
@@ -1,11 +1,11 @@
 var classsoftwarecontainer_1_1_software_container_config =
 [
     [ "SoftwareContainerConfig", "classsoftwarecontainer_1_1_software_container_config.html#a2f5e2b7451c4d077c7fb26580586214f", null ],
-    [ "SoftwareContainerConfig", "classsoftwarecontainer_1_1_software_container_config.html#aac7182acab18a236ef55a94b5fa0b1b4", null ],
+    [ "SoftwareContainerConfig", "classsoftwarecontainer_1_1_software_container_config.html#ac8c35ca815c9d2e6c601156791107879", null ],
     [ "~SoftwareContainerConfig", "classsoftwarecontainer_1_1_software_container_config.html#a749721c69716eb97b5ce356aa0ce236c", null ],
     [ "setEnableWriteBuffer", "classsoftwarecontainer_1_1_software_container_config.html#a88e960490be3166c4775bf3a227cc4ce", null ],
     [ "containerConfigPath", "classsoftwarecontainer_1_1_software_container_config.html#a61301234a0036fe3063841f82706e632", null ],
-    [ "containerRootDir", "classsoftwarecontainer_1_1_software_container_config.html#a90f43cc6a346453d1398fb3d49f97388", null ],
+    [ "sharedMountsDir", "classsoftwarecontainer_1_1_software_container_config.html#ab90d896ea466bdc396244b457f9d3ae1", null ],
     [ "containerShutdownTimeout", "classsoftwarecontainer_1_1_software_container_config.html#a039972439b8a6d056caab38571627e92", null ],
     [ "enableWriteBuffer", "classsoftwarecontainer_1_1_software_container_config.html#ae7d1ab47fecf06db8c59827491d4ca5c", null ]
 ];
