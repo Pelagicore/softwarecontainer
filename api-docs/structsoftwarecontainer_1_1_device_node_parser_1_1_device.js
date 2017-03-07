00033c35e42589d90999ab01491c9b549e181493
Jenkins <noreply@pelagicore.com>
2017-03-07 14:01:16 +0100

    Built new docs on master

--- api-docs/structsoftwarecontainer_1_1_device_node_parser_1_1_device.js
+++ api-docs/structsoftwarecontainer_1_1_device_node_parser_1_1_device.js
@@ -1,7 +1,5 @@
 var structsoftwarecontainer_1_1_device_node_parser_1_1_device =
 [
     [ "name", "structsoftwarecontainer_1_1_device_node_parser_1_1_device.html#aa5cdf2047e4973c9ff8ffcfce92ac5de", null ],
-    [ "major", "structsoftwarecontainer_1_1_device_node_parser_1_1_device.html#a558d075c39cfe86eae9ea303def46c3e", null ],
-    [ "minor", "structsoftwarecontainer_1_1_device_node_parser_1_1_device.html#a72d64d78b79fe090be3726acb91ef494", null ],
     [ "mode", "structsoftwarecontainer_1_1_device_node_parser_1_1_device.html#acae5572de26d55fac817bb94ca020856", null ]
 ];
