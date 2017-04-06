b7039077897c422527f3a6e55ecf10125bfa6b65
Jenkins <noreply@pelagicore.com>
2017-04-06 09:50:35 +0200

    Built new docs on master

--- api-docs/classsoftwarecontainer_1_1_hex_format_error.js
+++
@@ -1,7 +0,0 @@
-var classsoftwarecontainer_1_1_hex_format_error =
-[
-    [ "HexFormatError", "classsoftwarecontainer_1_1_hex_format_error.html#a499f5467a9864cd65d0af987d09af39a", null ],
-    [ "HexFormatError", "classsoftwarecontainer_1_1_hex_format_error.html#acbb5d00a2d02f4fa3af415abdab83d33", null ],
-    [ "what", "classsoftwarecontainer_1_1_hex_format_error.html#ad34f3a1b0d1dbc336b6c80889f3887f0", null ],
-    [ "m_message", "classsoftwarecontainer_1_1_hex_format_error.html#afad14d329d6a3e4b93a6d2dcd6f11d2f", null ]
-];
---
+++ api-docs/classsoftwarecontainer_1_1_invalid_input_error.js
@@ -0,0 +1,7 @@
+var classsoftwarecontainer_1_1_invalid_input_error =
+[
+    [ "InvalidInputError", "classsoftwarecontainer_1_1_invalid_input_error.html#ae8af4fd6360a94930004fa1af2242917", null ],
+    [ "InvalidInputError", "classsoftwarecontainer_1_1_invalid_input_error.html#aac4b12b473d23baae779975a18d543f6", null ],
+    [ "what", "classsoftwarecontainer_1_1_invalid_input_error.html#ad34f3a1b0d1dbc336b6c80889f3887f0", null ],
+    [ "m_message", "classsoftwarecontainer_1_1_invalid_input_error.html#afad14d329d6a3e4b93a6d2dcd6f11d2f", null ]
+];
