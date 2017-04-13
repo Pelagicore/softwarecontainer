76870ad8b6c8404a28519bbd6c14a54e37287d94
Therese Nordqvist <therese.nordqvist@pelagicore.com>
2017-04-13 15:52:08 +0200

    Add more test cases to verify and cover more cases for bindMount
    Signed-off-by: Therese Nordqvist <therese.nordqvist@pelagicore.com>

--- libsoftwarecontainer/component-test/filegateway_componenttest.cpp
+++ libsoftwarecontainer/component-test/filegateway_componenttest.cpp
@@ -23,6 +23,7 @@
 #include "functionjob.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <fstream>
 
 class FileGatewayTest : public SoftwareContainerGatewayTest
 {
@@ -37,27 +38,48 @@ public:
 
         gw = std::unique_ptr<FileGateway>(new FileGateway(m_container));
 
-        // Create file
-        ASSERT_TRUE(writeToFile(FILE_PATH, FILE_CONTENT));
+        // Create files
+        assert(mkdir(DIR_PATH.c_str(), 775) == 0);
+        assert(createFile(FILE_PATH, FILE_CONTENT));
+        assert(createFile(LONG_FILE_PATH, FILE_CONTENT_2));
+        assert(createFile(FILE_PATH_W_SPACES, FILE_CONTENT_2));
     }
 
     void TearDown() override
     {
         // Remove file
         unlink(FILE_PATH.c_str());
+        unlink(LONG_FILE_PATH.c_str());
+        unlink(FILE_PATH_W_SPACES.c_str());
+        rmdir(DIR_PATH.c_str());
 
         SoftwareContainerGatewayTest::TearDown();
     }
 
+    bool createFile(const std::string &path, const std::string &content)
+    {
+        std::ofstream out(path);
+        if (out.is_open()) {
+            out << content;
+            return out.good();
+        }
+        return false;
+    }
+
     const std::string FILE_CONTENT = "ahdkhqweuyreqiwenomndlaskmd";
     const std::string FILE_PATH = "/tmp/filename.txt";
+    const std::string DIR_PATH = "/tmp/testdir/";
+    const std::string LONG_FILE_PATH = DIR_PATH + "filename.txt";
+    const std::string FILE_CONTENT_2 = "bgsajbjkbjherkgbjhsbnjdfnjk";
+    const std::string FILE_W_SPACES = "example file name.txt";
+    const std::string FILE_PATH_W_SPACES = "/tmp/" + FILE_W_SPACES;
     const std::string CONTAINER_PATH = "/filename.txt";
 };
 
 /*
  * Test that a minimal conf is accepted and that activate works
  */
-TEST_F(FileGatewayTest, TestActivateWithMinimalValidConf) {
+TEST_F(FileGatewayTest, ActivateWithMinimalValidConf) {
     const std::string config =
     "["
         "{"
@@ -66,6 +88,119 @@ TEST_F(FileGatewayTest, TestActivateWithMinimalValidConf) {
         "}"
     "]";
     loadConfig(config);
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+    ASSERT_TRUE(gw->activate());
+}
+
+/*
+ * Test that bind mounting with a path which is malformed
+ * with too many "/" does not cause a failure.
+ */
+TEST_F(FileGatewayTest, MalformedPath) {
+    const std::string config =
+        "[{"
+        "  \"path-host\": \"//tmp//filename.txt\""
+        ", \"path-container\": \"///filename.txt\""
+        "}]";
+    loadConfig(config);
+
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+    ASSERT_TRUE(gw->activate());
+}
+
+/*
+ * Test that bind mounting with a path which does not exist
+ * causes the bind mounting to fail.
+ */
+TEST_F(FileGatewayTest, MountNonExisting) {
+    const std::string config =
+        "[{"
+            "  \"path-host\": \"/tmp/thisDirDoesNotExist/thisFileDoesNotExist.txt\""
+            ", \"path-container\": \"/tmp/thisFileDoesNotExist.txt\""
+        "}]";
+    loadConfig(config);
+
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+    ASSERT_FALSE(gw->activate());
+}
+
+/*
+ * Test that bind mounting with a destination path which is outside
+ * the /gateway dir is not allowed.
+ */
+TEST_F(FileGatewayTest, MountOutsideContainerDir) {
+    const std::string config =
+        "[{"
+            "  \"path-host\": \"" + FILE_PATH + "\""
+            ", \"path-container\": \".." + CONTAINER_PATH + "\""
+        "}]";
+    loadConfig(config);
+
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+    ASSERT_FALSE(gw->activate());
+}
+
+/*
+ * Test that bind mounting with a destination path which is the same
+ * as one already created is not allowed.
+ */
+TEST_F(FileGatewayTest, MountSameFilename) {
+    const std::string config =
+        "[{"
+            "  \"path-host\": \"" + FILE_PATH + "\""
+            ", \"path-container\": \"" + CONTAINER_PATH + "\""
+        "}]";
+    loadConfig(config);
+
+    EXPECT_TRUE(gw->setConfig(jsonConfig));
+
+    const std::string config2 =
+        "[{"
+            "  \"path-host\": \"" + LONG_FILE_PATH + "\""
+            ", \"path-container\": \"" + CONTAINER_PATH + "\""
+        "}]";
+    loadConfig(config2);
+
+    // Throws GatewayError
+    ASSERT_FALSE(gw->setConfig(jsonConfig));
+}
+
+/*
+ * Test that bind mounting with a destination path which is a file
+ * when the host path is a directory is not allowed.
+ */
+TEST_F(FileGatewayTest, MountDirAsFile) {
+    const std::string config =
+        "[{"
+            "  \"path-host\": \"" + FILE_PATH + "\""
+            ", \"path-container\": \"/app/testdir/\""
+        "}]";
+    loadConfig(config);
+
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+    ASSERT_FALSE(gw->activate());
+}
+
+/*
+ * Test that it is possible to bind mount a file name with spaces.
+ */
+TEST_F(FileGatewayTest, MountPathWithSpaces) {
+    const std::string FILE_W_SPACES = "example file name.txt";
+    const std::string FILE_PATH_W_SPACES = "/tmp/example file name.txt";
+    const std::string config =
+        "[{"
+            "  \"path-host\" : \"" + FILE_PATH + "\""
+            ", \"path-container\" : \"/" + FILE_W_SPACES + "\""
+        "}]";
+    loadConfig(config);
+    ASSERT_TRUE(gw->setConfig(jsonConfig));
+
+    const std::string config2 =
+        "[{"
+            "  \"path-host\" : \"" + FILE_PATH_W_SPACES + "\""
+            ", \"path-container\": \"" + CONTAINER_PATH + "\""
+        "}]";
+    loadConfig(config2);
     ASSERT_TRUE(gw->setConfig(jsonConfig));
     ASSERT_TRUE(gw->activate());
 }
